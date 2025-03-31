7ff16af575de4a355d61796e6b4b51585f33feb4
marmooo <marmooo@users.noreply.github.com>
2025-04-01 00:22:43 +0900

    update deps

--- build-dict.sh
+++ build-dict.sh
@@ -1,5 +1,5 @@
-deno run --allow-read --allow-write --allow-env game.js
-python reduce.py
-python -m pymagnitude.converter \
+deno run -RWE game.js
+rye run python reduce.py
+rye run python -m pymagnitude.converter \
   -i cc.ja.300-small.vec \
   -o cc.ja.300-small.magnitude
